7e6815973c33786ebe24c198abbc0b406415c59f
Anand Darshan <101782811+AnandHiggsBoson@users.noreply.github.com>
2022-11-27 12:16:24 +0530

    Add files via upload

---
+++ Income-Expense-Calculator/README.md
@@ -0,0 +1,12 @@
+# Income-Expense-Calculator
+
+Create a page whose title will be income-Expense Manager On this page, you have to create 
+three-button like income, expense and display.
+when you click on the income button, there should be some fields like 2 input fields and 1 add 
+button in these input fields you are to enter the sourse of income like salary 
+1000 and click on the add Button .
+after clicking it the alert box should open with the message like you added 1000.
+for salary. same process for the Expense button.
+After entering all the data in the income and Expense Button . When You click 
+on the Display Button. All The Data you Have added should be shown by the sequence on your page like income is whatever you entered.
+ here same for Expense data And also show the rest balance Done
---
+++ Income-Expense-Calculator/index.js
@@ -0,0 +1,110 @@
+var container= document.getElementsByClassName("container")[0];
+var listdiv = document.getElementsByClassName("list")[0];
+// console.log("hey");
+
+// var incdiv;
+let income=0;
+let expense=0;
+
+
+
+
+function addIncome(){
+
+    let input1= document.createElement("input");
+    let input2= document.createElement("input");
+    let incBtn= document.createElement("button");
+    let incDiv= document.createElement("div");
+    incDiv.className="incomeDiv";
+    
+    input1.style.margin="7px";
+    input2.style.margin="7px";
+    input1.placeholder="Income source";
+    input2.placeholder="Amount ";
+
+    input1.style.height="20px";
+    input2.style.height="20px";
+
+    incDiv.appendChild(input1);
+    incDiv.appendChild(input2);
+    incDiv.appendChild(incBtn);
+    // console.log("hey")
+    container.appendChild(incDiv);
+
+     
+    container.classList.add('addIncome');
+    incBtn.innerText="ADD";
+    incBtn.style.backgroundColor = 'Pink';
+    incBtn.addEventListener("click",()=>{
+    val1= input1.value;
+     val2= input2.value;
+     income+=Number(val2);
+        alert("You have added "+val2 +" for "+ val1);
+       
+    });
+
+}
+
+
+function addExpense(){
+    // incDiv.style.visibility="none";
+    let input1= document.createElement("input");
+    let input2= document.createElement("input");
+    let incBtn= document.createElement("button");
+    let incDiv= document.createElement("div");
+
+    input1.style.margin="7px";
+    input2.style.margin="7px";
+    input1.style.height="20px";
+    input2.style.height="20px";
+    input1.placeholder="Expense source";
+    input2.placeholder="Amount ";
+
+
+    incDiv.appendChild(input1);
+    incDiv.appendChild(input2);
+    incDiv.appendChild(incBtn);
+
+    container.appendChild(incDiv);
+    container.classList.add('addExpense');
+
+    incBtn.innerText="ADD";
+    incBtn.style.backgroundColor = 'Pink';
+    incBtn.addEventListener("click",function(){
+         val3= input1.value;
+         val4= input2.value;
+         expense+=Number(val4);
+        alert("You have added "+val4 +" for "+ val3);
+     
+    })
+
+}
+
+function display(){
+    container.style.display="none";
+    let displayIncome= document.createElement("div")
+    displayIncome.innerText="Total Income =" + income;
+    listdiv.appendChild(displayIncome);
+
+    let displayExpense= document.createElement("div")
+    displayExpense.innerText="Total Expense = " + expense;
+    
+    listdiv.appendChild(displayExpense);
+
+    let saving = document.createElement("h3");
+    saving.innerText= "TOTAL SAVING = " +(income- expense);
+    listdiv.appendChild(saving);
+
+    let resetDiv= document.createElement("div")
+        
+    let resetbtn= document.createElement("button");
+    resetbtn.innerText="Reset";
+    resetbtn.className="btn";
+    resetDiv.appendChild(resetbtn);
+    listdiv.appendChild(resetDiv);
+    listdiv.classList.add('addExpense');
+    resetbtn.addEventListener("click",function(){
+        console.log("hello");
+        location.reload();
+    })
+}
